4c3256c9ff4be6738d59224976c2b3cda5879e98
Abhishek <abhishekggsian@gmail.com>
2025-07-20 22:24:22 +0530

    3rd

--- Infra_todo/variable.tf
+++ Infra_todo/variable.tf
@@ -1,9 +1,24 @@
 variable "rg-name" {
+     type = map(object({
+          name     = string
+          location = string
+     }))
   
 }
 variable "vnet-name" {
-  
+     type = map(object({
+          name     = string
+          resource_group_name = string
+          address_space = list(string)
+          location = string
+     }))
 }
 variable "subnet-name" {
-  
+     type = map(object({
+          name     = string
+          location = string
+          resource_group_name = string
+          virtual_network_name = string
+          address_prefix = list(string)
+     }))
 }
