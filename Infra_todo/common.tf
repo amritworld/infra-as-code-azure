1c1a66ec99d40cee1785e0dfe0e0c6ea7d1dd18c
Abhishek <abhishekggsian@gmail.com>
2025-07-21 00:14:16 +0530

    5th

--- Infra_todo/common.tf
+++ Infra_todo/common.tf
@@ -6,14 +6,21 @@ module "rgs" {
 
 
 module "vnet" {
+  depends_on = [ module.rgs ]
   source    = "../modules/azurerm_virtual_network"
   vnet-name = var.vnet-name
 
 }
 
 module "subnet" {
+  depends_on = [ module.vnet ]
   source      = "../modules/azurerm_subnet"
   subnet-name = var.subnet-name
 
 }
 
+# module "nsg_module" {
+#   source    = "../modules/azure_nsg"
+#   nsg_name  = var.nsg_name
+  
+# }
