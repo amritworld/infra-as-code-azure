16f84939159594069eaa4b7e130bbf2525c0f4ec
Abhishek <abhishekggsian@gmail.com>
2025-07-20 21:42:06 +0530

    2nd

--- Infra_todo/provider.tf
+++ Infra_todo/provider.tf
@@ -6,6 +6,10 @@ terraform {
      }
      }
      backend "azurerm" {
+          resource_group_name = "abhi-rg"
+          storage_account_name = "stgabhi21"
+          container_name = "contabhi21"
+          key = "infra.tfstste"
         subscription_id = "b2744a0d-e7f7-4cea-af52-689c2e4a2c69"
         access_key = "w2PLbg7CGuPTtShrKWMRHWzWb8c628hD8NvVWp87pM8l+F3O6BAFRQOqF+EbIa3ibv0d7xdmBIVl+AStSOCxZQ=="  
        
